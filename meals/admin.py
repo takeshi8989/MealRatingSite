15d6bbe635050e9b700edf7ca813eb1d837f251a
Takeshi Hashimoto <htakeshi0614@gmail.com>
2022-11-25 17:54:10 -0800

    changed models

--- meals/admin.py
+++ meals/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Meal, MealRating
+from .models import Tag, Meal, MealRating
 
 admin.site.register(Meal)
 admin.site.register(MealRating)
+admin.site.register(Tag)
--- meals/models.py
+++ meals/models.py
@@ -1,20 +1,31 @@
 from django.db import models
 import datetime
 
-# Create your models here.
+
+class Tag(models.Model):
+    tagName = models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.tagName
+
+
 class Meal(models.Model):
     name = models.CharField(max_length=30)
     description = models.TextField()
     imgUrl = models.TextField()
     countryOfOrigin = models.CharField(max_length=30)
-    typicalMealTime = models.IntegerField(default=0)
     dateAdded = models.DateField(default=datetime.date.today)
     avgRating = models.FloatField(default=0)
     numOfVotes = models.IntegerField(default=0)
+    tags = models.ManyToManyField(Tag, blank=True)
 
+    def __str__(self):
+        return self.name
+    
     class Meta:
         db_table = 'meals'
 
+        
 class MealRating(models.Model):
     meal = models.ForeignKey(Meal, on_delete=models.CASCADE)
     rating = models.FloatField()
